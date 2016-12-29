d6fcae46eb44b7b5c6ce9818753517384b442410
Trishkin Vadim <halumein@gmail.com>
2016-12-29 16:57:23 +0500

    акции

--- src/MarketingBehavior.php
+++ src/MarketingBehavior.php
@@ -7,49 +7,137 @@ use yii\base\Behavior;
 
 class MarketingBehavior extends Behavior
 {
-    public $freeProduct = null;
-    public $targetModel = null;
-    public $presentCostAmount = null;
+    public $targetModel = null; // на что распростарняется поведение
+    public $freeProduct = null; // подарочный товар (['model' => '...', 'id' => '...'])
+    public $giftCostNeed = null; // сумма при покупке на которую выдаётся подарок
+    public $promoAction = null;
+    public $conditionProducts = null; // массив товаров для условия ([ ['model' => '...', 'id' => ...], ... ])
 
     public function events()
     {
         return [
-            'cart_update' => 'onNCostFreePresent',
+            'cart_update' => $this->promoAction,
         ];
     }
 
     public function init()
     {
         parent::init();
-
     }
 
-    public function onNCostFreePresent($event)
-    {
-        // var_dump('<=====================>');
+    /*
+    *   Акция "при определённой стоимости корзины что-то в подарок"
+    */
 
-        $cartElements = $event->cart;
+    public function onNCostFreeGift($event)
+    {
 
         $cartCount = $event->count;
         $cartCost = $event->cost;
 
-        $freeElement = false;
+        $freeElement = $this->checkProductInCart($event->cart, $this->freeProduct['model'], $this->freeProduct['id']);
 
-        foreach ($cartElements as $key => $element) {
-            if ($element->model == $this->freeProduct['model'] && $element->item_id == $this->freeProduct['id']) {
-                $freeElement = $element;
-                break;
-            }
-        }
-
-        if ($this->presentCostAmount != null && $cartCost > $this->presentCostAmount && $freeElement === false) {
+        if ($this->giftCostNeed != null && $cartCost > $this->giftCostNeed && $freeElement === false) {
+            // добавляем в корзину акциооный товар при выполнении условий акции
             $model = new $this->freeProduct['model'];
             yii::$app->cart->put($model::findOne($this->freeProduct['id']));
-        } elseif ($this->presentCostAmount != null && $cartCost < $this->presentCostAmount && $freeElement !== false) {
+        } elseif ($this->giftCostNeed != null && $cartCost < $this->giftCostNeed && $freeElement !== false) {
+            // если в корзине акционный товар есть - удаляем из корзины если условия акции не выполнены
             yii::$app->cart->deleteElement($freeElement);
         }
 
     }
+
+    /*
+    *   Акция "если в корзине есть перечисленные товары - что-то в подарок"
+    */
+
+    public function onNPlusNFreeGift($event)
+    {
+        $cartCount = $event->count;
+        $cartCost = $event->cost;
+
+        $freeElement = $this->checkProductInCart($event->cart, $this->freeProduct['model'], $this->freeProduct['id']);
+
+        $condition = 0; // условие - количество определённых позиций в корзине должно быть равно условию. изначально не равно
+        if ($this->conditionProducts) {
+            foreach ($this->conditionProducts as $key => $conditionProduct) {
+                if ($this->checkProductInCart($event->cart, $conditionProduct['model'], $conditionProduct['id'])) {
+                    $condition++;
+                }
+            }
+            if ($condition === count($this->conditionProducts) && $freeElement === false) {
+                // добавляем в корзину акциооный товар при выполнении условий акции
+                $model = new $this->freeProduct['model'];
+                yii::$app->cart->put($model::findOne($this->freeProduct['id']));
+            } elseif ($condition !== count($this->conditionProducts) && $freeElement !== false) {
+                // если в корзине акционный товар есть - удаляем из корзины если условия акции не выполнены
+                yii::$app->cart->deleteElement($freeElement);
+            }
+        }
+    }
+
+    /*
+    *   Акция "если в корзине есть определённое количество указанного товара - что-то в подарок"
+    */
+
+    public function onAmountNFreeGift($event)
+    {
+        $cartCount = $event->count;
+        $cartCost = $event->cost;
+
+        $freeElement = $this->checkProductInCart($event->cart, $this->freeProduct['model'], $this->freeProduct['id']);
+
+        if ($this->conditionProducts) {
+            foreach ($this->conditionProducts as $key => $conditionProduct) {
+                if ($element = $this->checkProductInCart($event->cart, $conditionProduct['model'], $conditionProduct['id'])) {
+                    if ($element->count >= $conditionProduct['count'] && $freeElement === false) {
+                        // добавляем в корзину акциооный товар при выполнении условий акции
+                        $model = new $this->freeProduct['model'];
+                        yii::$app->cart->put($model::findOne($this->freeProduct['id']));
+                    } elseif ($element->count < $conditionProduct['count'] && $freeElement !== false) {
+                        // если в корзине акционный товар есть - удаляем из корзины если условия акции не выполнены
+                        yii::$app->cart->deleteElement($freeElement);
+                    } elseif ($element->count >= $conditionProduct['count'] && $freeElement !== false) {
+                        // если
+                        if ((integer)($element->count/$conditionProduct['count'] !== $element->count)) {
+                            //
+                            $count = (integer)($element->count/$conditionProduct['count']);
+                            $freeElement->count = $count;
+                            $freeElement->update();
+
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+
+
+    /*
+    *   Проверяет есть ли продукт в корзине
+    *   @property array $cartElements - корзина
+    *   @property string $productModel - модель
+    *   @property int $productId - ид
+    *   @return - bool/object
+    */
+
+    private function checkProductInCart($cartElements, $productModel, $productId)
+    {
+        $freeElement = false; // акционного товара в корзине нет
+
+        // TODO привязаться на $this->targetModel
+        foreach ($cartElements as $key => $element) {
+            if ($element->model == $productModel && $element->item_id == (int)$productId) {
+                $freeElement = $element; // акционный товар в корзине есть
+                break;
+            }
+        }
+
+        return $freeElement;
+    }
+
 
 
 }
